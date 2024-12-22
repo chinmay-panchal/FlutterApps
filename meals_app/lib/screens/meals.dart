4810c0d444310d5a4c0844a94521abb0d1e4dfdd
Chinmay Panchal <Chinmaypanchalcollab@gmail.com>
2024-12-22 17:25:06 +0530

    meals_app completed

---
+++ meals_app/lib/screens/meals.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/blueprints/meal.dart';
+import 'package:meals_app/screens/meal_details.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    super.key,
+    this.title,
+    required this.meals,
+    required this.onToggleFavorite,
+  });
+
+  final String? title;
+  final List<Meal> meals;
+  final void Function(Meal meal) onToggleFavorite;
+
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(
+          meal: meal,
+          onToggleFavorite: onToggleFavorite,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh oh ... nothing here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try selecting a different category!',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(
+          meal: meals[index],
+          onSelectItem: (meal) {
+            selectMeal(context, meal);
+          },
+        ),
+      );
+    }
+
+    if (title == null) { // we are in the tabs screen and clicked on the favourite screen and already we have a title same for the category screen by default no title
+      return content;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title!),
+      ),
+      body: content,
+    );
+  }
+}
